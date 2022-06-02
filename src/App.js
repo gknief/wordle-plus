28e15852593615921ce91c157a3362a968361f9b
Grant Knief <gknief@mac.com>
2022-06-01 20:41:12 -0400

    created function to fetch random word

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions6')
+      .then(res => res.json())
+      .then(json => {
+        // generate random num
+        const randomWord = json[Math.floor(Math.random() * json.length)];
+        console.log(randomWord);
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1>Wordle+</h1>
